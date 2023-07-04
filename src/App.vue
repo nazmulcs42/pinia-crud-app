164588a50dc5148f19e09fa1711f40e3d2dc802d
nazmulcs42 <nazmul.islam.lotif@gmail.com>
2023-07-04 22:00:16 +0600

    git initial commits

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <h1>Pinia State Management</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
